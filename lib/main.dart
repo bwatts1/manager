026185b67abb13a70c8f53dd3b7a330f80c9e45e
bwatts1 <wattsbraydon2002@gmail.com>
2025-10-21 17:12:25 -0400

    using a previous project as a template for sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,151 @@
 import 'package:flutter/material.dart';
+import 'database_helper.dart';
 
-void main() {
+// Here we are using a global variable. You can use something like
+// get_it in a production app.
+final dbHelper = DatabaseHelper();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize the database
+  await dbHelper.init();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SQFlite Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final TextEditingController _idController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _showAlert(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(onPressed: () => Navigator.pop(context), child: const Text('OK')),
+        ],
+      ),
+    );
+  }
+
+  void _insert() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnName: _nameController.text,
+      DatabaseHelper.columnAge: int.parse(_ageController.text),
+    };
+    final id = await dbHelper.insert(row);
+    debugPrint('inserted row id: $id');
+    _showAlert('Insert', 'Inserted row id: $id');
+  }
+
+  void _query() async {
+    final id = int.parse(_idController.text);
+    final row = await dbHelper.queryById(id);
+    debugPrint('query row: $row');
+    _showAlert('Query', row != null ? row.toString() : 'No row found with ID $id');
+  }
+
+  void _update() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: int.parse(_idController.text),
+      DatabaseHelper.columnName: _nameController.text,
+      DatabaseHelper.columnAge: int.parse(_ageController.text),
+    };
+    final rowsAffected = await dbHelper.update(row);
+    debugPrint('updated $rowsAffected row(s)');
+    _showAlert('Update', 'Updated $rowsAffected row(s)');
+  }
+
+  void _delete() async {
+    final id = int.parse(_idController.text);
+    final rowsDeleted = await dbHelper.delete(id);
+    debugPrint('deleted $rowsDeleted row(s): row $id');
+    _showAlert('Delete', 'Deleted $rowsDeleted row(s): row $id');
+  }
+
+  void _queryById() async {
+    final idToQuery = int.parse(_idController.text);
+    final row = await dbHelper.queryById(idToQuery);
+    if (row != null) {
+      debugPrint('Queried row: $row');
+      _showAlert('Queried Row', '''
+ID: ${row[DatabaseHelper.columnId]}
+Name: ${row[DatabaseHelper.columnName]}
+Age: ${row[DatabaseHelper.columnAge]}
+''');
+    } else {
+      debugPrint('No row found with ID $idToQuery');
+      _showAlert('Query by ID', 'No row found with ID $idToQuery');
+    }
+  }
+
+  void _deleteAll() async {
+    final rowsDeleted = await dbHelper.deleteAll();
+    debugPrint('Deleted $rowsDeleted row(s)');
+    _showAlert('Delete All', 'Deleted $rowsDeleted row(s)');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: const Text('sqflite')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            TextField(
+              controller: _idController,
+              decoration: const InputDecoration(labelText: 'ID'),
+              keyboardType: TextInputType.number,
             ),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: _ageController,
+              decoration: const InputDecoration(labelText: 'Age'),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: _insert, child: const Text('insert')),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _query, child: const Text('query')),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _update, child: const Text('update')),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _delete, child: const Text('delete')),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _queryById, child: const Text('query by ID')),
+            const SizedBox(height: 10),
+            ElevatedButton(onPressed: _deleteAll, child: const Text('delete all')),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
